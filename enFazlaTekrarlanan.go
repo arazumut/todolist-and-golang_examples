78287fdef0dacf99033945b5cf7adb263d58eb4f
K.Umut Araz <150933483+arazumut@users.noreply.github.com>
2024-06-12 14:33:00 +0300

    Add files via upload

---
+++ enFazlaTekrarlanan.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const MAX_CHARACTERS = 200
+
+func main() {
+	var metin string
+	karakterSayisi := make(map[rune]int)
+	var maxTekrar int
+	var enCokTekrarlananKarakter rune
+
+	fmt.Print("Metni giriniz (en fazla 200 karakter): ")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		metin = scanner.Text()
+	}
+
+	// Metindeki her bir karakterin sayısını say
+	for _, karakter := range metin {
+		if karakter != '\n' { // Satır sonu karakterini sayma
+			karakterSayisi[karakter]++
+		}
+	}
+
+	// En fazla tekrarlanan karakteri bul
+	for karakter, sayi := range karakterSayisi {
+		if sayi > maxTekrar {
+			maxTekrar = sayi
+			enCokTekrarlananKarakter = karakter
+		}
+	}
+
+	fmt.Printf("En fazla tekrarlanan karakter: %c\n", enCokTekrarlananKarakter)
+	fmt.Printf("Tekrar sayısı: %d\n", maxTekrar)
+}
