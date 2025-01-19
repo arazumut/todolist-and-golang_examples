9f282406d7e94ca12f9bddfff513933492a31768
K.Umut Araz <150933483+arazumut@users.noreply.github.com>
2025-01-19 23:21:09 +0300

    Update kelimeBul.go

--- kelimeBul.go
+++ kelimeBul.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 func main() {
-	var kelime string
+    var kelime string
 
-	fmt.Print("Lütfen bir kelime giriniz: ")
-	fmt.Scanln(&kelime)
+    fmt.Print("Lütfen bir kelime giriniz: ")
+    fmt.Scanln(&kelime)
 
-	uzunluk := len(kelime)
-	kelime2 := make([]byte, uzunluk)
+    uzunluk := len(kelime)
+    kelime2 := make([]byte, uzunluk)
 
-	for j := 0; j < uzunluk/2; j++ {
-		gecici := kelime[uzunluk-j-1]
-		kelime2[uzunluk-j-1] = kelime[j]
-		kelime2[j] = gecici
-	}
+    for j := 0; j < uzunluk; j++ {
+        kelime2[j] = kelime[uzunluk-j-1]
+    }
 
-	fmt.Printf("Girilen kelimenin tersi: %s\n", kelime2)
+    fmt.Printf("Girilen kelimenin tersi: %s\n", kelime2)
 }
