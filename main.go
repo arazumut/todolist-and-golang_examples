49064f05973c1dd90fc81c822b3d7a8cac53f278
K.Umut Araz <150933483+arazumut@users.noreply.github.com>
2025-01-19 23:22:16 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,46 +1,39 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"time"
+    "log"
+    "net/http"
+    "time"
 
-	"example.com/todo-app-go/internal/api"
-	"example.com/todo-app-go/internal/auth"
-	"example.com/todo-app-go/internal/mock"
+    "example.com/todo-app-go/internal/api"
+    "example.com/todo-app-go/internal/auth"
+    "example.com/todo-app-go/internal/mock"
 )
 
 func main() {
+    // Created By K.Umut Araz
+    
+    mockService := mock.NewMockService()
+    authService := auth.NewAuthService()
+    apiHandler := api.NewAPIHandler(mockService, authService)
 
+    // Yönlendirilecek rotaları tanımla
+    http.HandleFunc("/login", apiHandler.Login)
+    http.HandleFunc("/todo/create", apiHandler.CreateToDoList)
+    http.HandleFunc("/todo/delete", apiHandler.DeleteToDoList)
+    http.HandleFunc("/todo/addmessage", apiHandler.AddToDoMessage)
+    http.HandleFunc("/todo/deletemessage", apiHandler.DeleteToDoMessage)
+    http.HandleFunc("/todo/updatemessage", apiHandler.UpdateToDoMessage)
+    http.HandleFunc("/todo/get", apiHandler.GetToDoLists)
 
-// Created By K.Umut Araz
-	
-	
-	mockService := mock.NewMockService()
+    // HTTP sunucusunu başlat
+    server := &http.Server{
+        Addr:         ":8080",
+        Handler:      nil, // DefaultMux kullan
+        ReadTimeout:  10 * time.Second,
+        WriteTimeout: 10 * time.Second,
+    }
 
-	
-	authService := auth.NewAuthService()
-
-	
-	apiHandler := api.NewAPIHandler(mockService, authService)
-
-	// Yönlendirilecek rotaları tanımla
-	http.HandleFunc("/login", apiHandler.Login)
-	http.HandleFunc("/todo/create", apiHandler.CreateToDoList)
-	http.HandleFunc("/todo/delete", apiHandler.DeleteToDoList)
-	http.HandleFunc("/todo/addmessage", apiHandler.AddToDoMessage)
-	http.HandleFunc("/todo/deletemessage", apiHandler.DeleteToDoMessage)
-	http.HandleFunc("/todo/updatemessage", apiHandler.UpdateToDoMessage)
-	http.HandleFunc("/todo/get", apiHandler.GetToDoLists)
-
-	// HTTP sunucusunu başlat
-	server := &http.Server{
-		Addr:         ":8080",
-		Handler:      nil, // DefaultMux kullan
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
-
-	log.Println("Server listening on port 8080...")
-	log.Fatal(server.ListenAndServe())
+    log.Println("Server listening on port 8080...")
+    log.Fatal(server.ListenAndServe())
 }
